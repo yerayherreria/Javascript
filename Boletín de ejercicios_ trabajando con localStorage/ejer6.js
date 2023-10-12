65cfd64c7849f1e8b1eb91b6655f6c5fe41cbf0b
yerayherreria <yerayherre@gmail.com>
2023-10-12 13:01:25 +0200

    Subiendo

--- Boletín de ejercicios_ trabajando con localStorage/ejer6.js
+++ Boletín de ejercicios_ trabajando con localStorage/ejer6.js
@@ -1,5 +1,5 @@
 //localStorage.removeItem('users');
-
+/*
 let anadir = document.querySelector('button');
 let lista=document.getElementById('userList');
 
@@ -65,7 +65,7 @@ function anniadirUser(nombre,direccion,email){
     nuevaP.appendChild(botonBorrar);
     lista.appendChild(nuevaP);
 }
-verUsers(localStorage.getItem('users'));
+verUsers(localStorage.getItem('users'));*/
 /*
 //Eliminar Usuarios
 let borrar = document.querySelectorAll('#borrar');
@@ -75,3 +75,78 @@ for(let i=0;i<borrar.length;i++){
         
     })
 }*/
+
+let anadir = document.querySelector("button[type='submit']");
+let lista=document.getElementById('userList');
+
+let nombre = document.getElementById('name');
+let direccion = document.getElementById('address');
+let email = document.getElementById('email');
+
+//Para almacenar en el localStorage
+let usuarios = localStorage.getItem("users") == null ? [] :JSON.parse(localStorage.getItem("users")); 
+let copiaUsuariosString = localStorage.getItem("users") == null ? [] : localStorage.getItem("users"); 
+
+anadir.addEventListener(`click`,function(){ 
+    if(nombre.value != "" && direccion.value != "" && email.value != ""){ 
+        let usuario = {nombre:nombre.value, direccion:direccion.value, email:email.value}; 
+    
+        usuarios.push(usuario); 
+        localStorage.setItem("users",JSON.stringify(usuarios)); 
+        
+    }
+});
+
+mostrarUsuarios();
+borrar();
+modificar();
+
+function mostrarUsuarios(){ 
+    usuarios.forEach(usuario => { 
+        let li = document.createElement("li"); 
+        li.appendChild(document.createTextNode(`${usuario.nombre} : ${usuario.direccion} : ${usuario.email} : `));
+
+        let editar = document.createElement("button"); 
+        editar.textContent = "Editar"; 
+        editar.setAttribute("id","mod"); 
+
+        let br = document.createElement("br"); 
+
+        let borrar = document.createElement("button"); 
+        borrar.textContent = "Borrar"; 
+        borrar.setAttribute("id","del"); 
+
+        li.appendChild(editar); 
+        li.appendChild(br); 
+        li.appendChild(borrar); 
+        lista.appendChild(li); 
+    });
+}
+
+function borrar(){
+    let borrar = document.querySelectorAll("#del"); 
+    for(let i = 0; i < borrar.length; i++){ 
+        borrar[i].addEventListener(`click`,function(event){ 
+            event.target.parentNode.remove();
+            usuarios.splice(i,1); 
+            localStorage.setItem("users",JSON.stringify(usuarios));
+        });
+    }
+}
+
+function modificar(){
+    let editar = document.querySelectorAll("#mod"); 
+    for(let o = 0; o < editar.length ; o++){ 
+        editar[o].addEventListener(`click`,function(event){ 
+        let usuarioRecuperado = usuarios[o]; 
+        event.target.parentNode.remove();  
+        usuarios.splice(o,1); 
+    
+        nombre.value = usuarioRecuperado.nombre;
+        direccion.value = usuarioRecuperado.direccion; 
+        email.value = usuarioRecuperado.email; 
+    
+        enviar.textContent = "Editar usuario"; 
+        })
+    }
+}
